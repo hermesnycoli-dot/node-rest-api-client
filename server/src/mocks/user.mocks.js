734d82dc467ab3e3856c01eade7c9fec9735a183
nicoli hermes <hermesnycoli@gmail.com>
2026-07-12 11:33:38 -0300

    feat:implementar dados simulados

---
+++ server/src/mocks/user.mocks.js
@@ -0,0 +1,18 @@
+  const users = [{
+   id: 1,
+   name:'alex Bessa',
+   email: 'bessa@email.com',
+   status: true
+}, {  
+    id:2,
+   name:'Valdiana Bessa',
+   email: 'Valdiana@gmail.com',
+   status: true
+}, { 
+    id: 3 ,
+   name:'Ana Bessa',
+   email: 'ana@gmail.com',
+   status: false
+
+ }]; 
+    export default users;
