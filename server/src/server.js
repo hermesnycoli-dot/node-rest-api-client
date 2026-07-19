867824aa9dd37788f1d3a2ed7292968c52d75ff0
nicoli hermes <hermesnycoli@gmail.com>
2026-07-19 10:55:16 -0300

    feat:adicionar respostas para pagina nao encontrada

--- server/src/server.js
+++ server/src/server.js
@@ -4,5 +4,9 @@ import routerUsers from './routes/user.route.js';
 http.createServer((request, response) => {
      if (request.url === '/users') {
         return routerUsers(request, response);
+        return;
+
          }
+         response.writeHead(404, {'content-type': 'application/json'});
+         response.end(JSON.stringify({message:'página não encontrada'}));
 }).listen(3000);
